d4bce1d9429051f81955e24f8ef5ea0a92de3d52
Adam03lvl <adam.benkhazzi@outlook.com>
2025-10-25 11:42:24 +0200

    removed unnessecary imports

--- glitchKIT/scripts/InputComponent.cs
+++ glitchKIT/scripts/InputComponent.cs
@@ -1,11 +1,9 @@
 using Godot;
-using System;
 
 public partial class InputComponent : Node
 {
 	public float inputHorizontal = 0.0f;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		inputHorizontal = Input.GetAxis("move_left", "move_right");
