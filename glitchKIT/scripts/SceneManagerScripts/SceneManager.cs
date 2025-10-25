f21c6feadb8fc08eeba71a9aa10893aa3d02e1a7
sqmmi3 <samip.shrestha@student.ucll.be>
2025-10-25 23:52:00 +0200

    sceneManager

--- glitchKIT/scripts/SceneManagerScripts/SceneManager.cs
+++ glitchKIT/scripts/SceneManagerScripts/SceneManager.cs
@@ -12,8 +12,8 @@ public partial class SceneManager : Node
 	public static SceneManager instance;
 	
 	public Dictionary<eSceneNames, SceneData> sceneDictionary = new Dictionary<eSceneNames, SceneData>() {
-		{eSceneNames.Main, new SceneData("res://Scenes/MainMenuScene/menuScene.tscn", "Main menu")},
-		{eSceneNames.Other, new SceneData("res://Scenes/scene/World.tscn", "other")},
+		{eSceneNames.Main, new SceneData("res://Scenes/menuScene.tscn", "Main menu")},
+		{eSceneNames.Other, new SceneData("res://Scenes/World.tscn", "other")},
 	};
 	
 	public override void _Ready()
