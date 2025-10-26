123c40ef15409a651b3c41a2ca4be19478c677f0
Adam03lvl <adam.benkhazzi@outlook.com>
2025-10-26 11:51:28 +0100

    reset game state after death

--- glitchKIT/scripts/Player.cs
+++ glitchKIT/scripts/Player.cs
@@ -12,29 +12,40 @@ public partial class Player : CharacterBody2D
   [Export]
   public AnimationComponent animationComponent;
 
+  public Area2D key;
+
   public float spawnX = -250; 
   public float spawnY = 50; 
   public float deathY = 175; 
   public bool shouldDie = false;
+  public bool hasKey = false;
 
   float time = 0;
-
   float timer(double delta, float a){
 	return a += 1 * (float) delta;
+  }
+
+  public override void _Ready(){
+	key = GetNode<Area2D>("../Key");
+  }
+
+  public void reset(){
+	  Position = new Vector2(spawnX, spawnY);
+	  time = 0;
+	  shouldDie = false;
+	  hasKey = false;
   }
 
   public void handleDeath(double delta){
 	if(Position.Y > deathY) shouldDie = true;
 
-	if(shouldDie){
-	  Position = new Vector2(Position.X, deathY+50);
-	  time = timer(delta,  time);
-	  if  (time > 2.0f){
-		Position = new Vector2(spawnX, spawnY);
-		time = 0;
-	  }
-	  shouldDie = false;
+  if(shouldDie){
+	Position = new Vector2(Position.X, deathY+50);
+	time = timer(delta,  time);
+	if  (time > 2.0f){
+	  reset();
 	}
+  }
   }
 
   public override void _PhysicsProcess(double delta)
@@ -44,9 +55,12 @@ public partial class Player : CharacterBody2D
 	animationComponent.handleMoveAnimation(inputComponent.inputHorizontal);
 	animationComponent.handleJumpAnimation(movementComponent.IsJumping);
 	movementComponent.HandleJump(this, inputComponent.getJumpInput(), gravityComponent.Gravity);
+
+  if(key.OverlapsBody(this)){
+    hasKey = true;
+  }
+
   handleDeath(delta);
-
-
 	MoveAndSlide();
   }
 
