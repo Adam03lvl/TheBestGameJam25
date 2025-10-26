84d64d56c37d78aa46027ec01a3700714fbc2c4a
Adam03lvl <adam.benkhazzi@outlook.com>
2025-10-26 15:26:22 +0100

    added screen bangs n that

--- glitchKIT/scripts/Player.cs
+++ glitchKIT/scripts/Player.cs
@@ -2,82 +2,83 @@ using Godot;
 
 public partial class Player : CharacterBody2D
 {
-	[ExportSubgroup("Nodes")]
-	[Export]
-	public GravityComponent gravityComponent;
-	[Export]
-	public InputComponent inputComponent;
-	[Export]
-	public MovementComponent movementComponent;
-	[Export]
-	public AnimationComponent animationComponent;
+  [ExportSubgroup("Nodes")]
+  [Export]
+  public GravityComponent gravityComponent;
+  [Export]
+  public InputComponent inputComponent;
+  [Export]
+  public MovementComponent movementComponent;
+  [Export]
+  public AnimationComponent animationComponent;
 
-	public Area2D key;
-	public Area2D spikes;
+  public Area2D key;
+  public Area2D spikes;
 
-	public float spawnX = -250;
-	public float spawnY = 50;
-	public float deathY = 175;
-	public bool shouldDie = false;
-	public bool hasKey = false;
+  public float spawnX = -250;
+  public float spawnY = 50;
+  public float deathY = 175;
+  public bool shouldDie = false;
+  public bool isDying= false;
+  public bool hasKey = false;
 
-	float time = 0;
-	float timer(double delta, float a)
+  float time = 0;
+  float timer(double delta, float a)
+  {
+	return a += 1 * (float)delta;
+  }
+
+  public override void _Ready()
+  {
+	key = GetNode<Area2D>("../Key");
+	spikes = GetNode<Area2D>("../Spikes");
+  }
+
+  public void reset()
+  {
+	Position = new Vector2(spawnX, spawnY);
+	time = 0;
+	shouldDie = false;
+	hasKey = false;
+  }
+
+  public void handleDeath(double delta)
+  {
+
+	if (Position.Y > deathY) shouldDie = true;
+
+	if (shouldDie && !isDying)
 	{
-		return a += 1 * (float)delta;
+	  isDying = true;
+	  SceneManager.instance.ShowImage("res://assets/death.png", .5f, .2f);
+	  Position = new Vector2(Position.X, deathY + 50);
+	  GetTree().CreateTimer(.7f).Timeout += ()=> {
+		reset();
+		isDying = false;
+	  };
+	}
+  }
+
+  public override void _PhysicsProcess(double delta)
+  {
+	gravityComponent.handleGravity(this, delta);
+	movementComponent.handleHorizontalMovement(this, inputComponent.inputHorizontal);
+	animationComponent.handleMoveAnimation(inputComponent.inputHorizontal);
+	animationComponent.handleJumpAnimation(movementComponent.IsJumping);
+	movementComponent.HandleJump(this, inputComponent.getJumpInput(), gravityComponent.Gravity);
+
+	if (spikes.OverlapsBody(this)) shouldDie = true;
+
+	if (!hasKey && key.OverlapsBody(this))
+	{
+	  movementComponent.stopMoving = true;
+	  SceneManager.instance.ShowImage("res://assets/Keycollected.png", .5f, .2f);
+	  GetTree().CreateTimer(1.0f).Timeout += ()=> movementComponent.stopMoving=false;
+	  hasKey = true;
 	}
 
-	public override void _Ready()
-	{
-		key = GetNode<Area2D>("../Key");
-		spikes = GetNode<Area2D>("../Spikes");
-	}
-
-	public void reset()
-	{
-		Position = new Vector2(spawnX, spawnY);
-		time = 0;
-		shouldDie = false;
-		hasKey = false;
-	}
-
-	public void handleDeath(double delta)
-	{
-
-
-		if (Position.Y > deathY) shouldDie = true;
-
-		if (shouldDie)
-		{
-			Position = new Vector2(Position.X, deathY + 50);
-			time = timer(delta, time);
-			if (time > 2.0f)
-			{
-				reset();
-			}
-		}
-	}
-
-	public override void _PhysicsProcess(double delta)
-	{
-		gravityComponent.handleGravity(this, delta);
-		movementComponent.handleHorizontalMovement(this, inputComponent.inputHorizontal);
-		animationComponent.handleMoveAnimation(inputComponent.inputHorizontal);
-		animationComponent.handleJumpAnimation(movementComponent.IsJumping);
-		movementComponent.HandleJump(this, inputComponent.getJumpInput(), gravityComponent.Gravity);
-
-		if (spikes.OverlapsBody(this))
-		{
-			shouldDie = true;
-		}
-
-		if (key.OverlapsBody(this))
-		{
-			hasKey = true;
-		}
-
-		handleDeath(delta);
-		MoveAndSlide();
-	}
+	handleDeath(delta);
+	MoveAndSlide();
+  }
 
 }
--- glitchKIT/scripts/SceneManagerScripts/SceneManager.cs
+++ glitchKIT/scripts/SceneManagerScripts/SceneManager.cs
@@ -4,14 +4,65 @@ using System.Collections.Generic;
 
 public partial class SceneManager : Node
 {
-	public static SceneManager instance;
-	
-	public override void _Ready()
-	{
-		instance = this;
-	}
+  public static SceneManager instance;
 
-	public void ChangeScene(String sceneName) {
-		GetTree().ChangeSceneToFile($"res://Scenes/{sceneName}.tscn");
-	}
+  private CanvasLayer _uiLayer;
+  private TextureRect _keyImage;
+  private Tween _currentTween;
+
+  public override void _Ready()
+  {
+    instance = this;
+
+    _uiLayer = new CanvasLayer();
+    _uiLayer.Layer = 100;
+    AddChild(_uiLayer);
+
+    _keyImage = new TextureRect();
+    _keyImage.Visible = false;
+
+    _keyImage.SetAnchorsPreset(Control.LayoutPreset.FullRect);
+
+    _keyImage.ExpandMode = TextureRect.ExpandModeEnum.IgnoreSize;
+    _keyImage.StretchMode = TextureRect.StretchModeEnum.Scale;
+
+    _uiLayer.AddChild(_keyImage);
+  }
+
+  public void ShowImage(string imagePath, float displayTime = 2.0f, float fadeOutTime = 0.5f)
+  {
+    if (_currentTween != null && _currentTween.IsValid())
+    {
+      _currentTween.Kill();
+    }
+    var texture = GD.Load<Texture2D>(imagePath);
+    if (texture == null)
+    {
+      GD.PrintErr($"Failed to load image at path: {imagePath}");
+      return;
+    }
+    _keyImage.Texture = texture;
+    _keyImage.Modulate = new Color(1, 1, 1, 1); // Reset alpha to fully opaque
+    _keyImage.Visible = true;
+    _currentTween = CreateTween();
+    _currentTween.TweenInterval(displayTime);
+    _currentTween.TweenProperty(_keyImage, "modulate:a", 0.0f, fadeOutTime);
+    _currentTween.TweenCallback(Callable.From(() => _keyImage.Visible = false)); 
+  }
+
+  public void HideImage(float fadeTime = 0.5f)
+  {
+    if (_currentTween != null && _currentTween.IsValid())
+    {
+      _currentTween.Kill();
+    }
+
+    var tween = CreateTween();
+    tween.TweenProperty(_keyImage, "modulate:a", 0.0f, fadeTime);
+    tween.TweenCallback(Callable.From(() => _keyImage.Visible = false));
+  }
+
+  public void ChangeScene(String sceneName) {
+    GetTree().ChangeSceneToFile($"res://Scenes/{sceneName}.tscn");
+  }
 }
